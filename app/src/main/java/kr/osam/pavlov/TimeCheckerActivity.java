8983d08a1603c41f66eec544769b8d43f96948e8
steinjun0 <1142308@naver.com>
2019-10-22 03:40:04 +0900

    screenReceiver 구현 헷갈리지 않기! screenReceiver는 Screen On / Off Receiver의 BroadCast를 Activity에서 받아주는 Receiver이다. 받은 intent에는 Screen On / Off Receiver의 onReceive 시간이 전달된다. 이를 통해 화면의 꺼진 시간을 계산하고 그만큼 FinishTime을 늦춰서 화면이 꺼진시간을 카운트를 하지 않는다.

--- app/src/main/java/kr/osam/pavlov/TimeCheckerActivity.java
+++ app/src/main/java/kr/osam/pavlov/TimeCheckerActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -35,6 +36,7 @@ public class TimeCheckerActivity extends AppCompatActivity {
     AlarmManager mAlarmManager;
 
     Intent receiverServiceIntent;
+    ScreenReceiver screenReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +48,17 @@ public class TimeCheckerActivity extends AppCompatActivity {
         buttonSetAlarm = (Button) findViewById(R.id.buttonSetAlarm);
         buttonSetFinish = (Button) findViewById(R.id.buttonSetFinish);
 
+        screenReceiver = new ScreenReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ScreenOffReceiver.SCREEN_OFF_NOTIFICATION);
+        filter.addAction(ScreenOnReceiver.SCREEN_ON_NOTIFICATION);
+        filter.addAction(AlarmReceiver.MISSTION_FAIL_NOTIFICATION);
+        filter.addAction(AlarmReceiver.MISSTION_SUCCESS_NOTIFICATION);
+        this.registerReceiver(screenReceiver, filter);
+
         receiverServiceIntent = new Intent(this, ScreenReceiverService.class);
         startService(receiverServiceIntent);
     }
-
-
-
 
     public void onClickSetAlarm(View view){
 
@@ -178,4 +185,122 @@ public class TimeCheckerActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    public class ScreenReceiver extends BroadcastReceiver {
+        long time = 0;
+        long presentOnTime = 0;
+        long presentOffTime = 0;
+        long durationOff = 0;
+        long durationOn = 0;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("test", "Custom Broadcast OnReceive");
+            //화면 켜짐/꺼짐, 미션 성공/실패 분기
+            if (ScreenOnReceiver.SCREEN_ON_NOTIFICATION.equals(intent.getAction())) {
+                //intent로 ScreenOn 정보 받기
+                presentOnTime = intent.getLongExtra("presentTime", 0 );
+                String onLatest = "On Latest" + intent.getStringExtra("present");
+                String onPast = "On " + intent.getStringExtra("past");
+                //화면에 띄우기
+                //textScreenOn.setText(onLatest);
+                // textPastScreenOn.setText(onPast);
+
+                Log.d("test", intent.getStringExtra("name")+" onReceive");
+                //저장된 사용시간 불러오기
+                SimpleDateFormat sdf = new SimpleDateFormat("MM / dd / HH:mm:ss");
+                SharedPreferences pref = getSharedPreferences("preference", MODE_PRIVATE);
+                if(time == 0) {
+                    time = pref.getLong("setTime", 0);
+                    Log.d("test_load_setTime", "setTime = " + sdf.format(time));
+                }
+                //화면 꺼져 있던 시간 구하기
+                if(presentOffTime != 0){
+                    durationOff = presentOnTime - presentOffTime;
+                }
+                Log.d("test_duration_Off", sdf.format(durationOff));
+
+                //알람을 위한 Intent, PendingIntent 선언
+                Intent mAlarmIntent = new Intent("com.example.mission1.receiver.ALARM_ON");
+                mAlarmIntent.putExtra("flag","fail");
+                PendingIntent mPendingIntent =
+                        PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                0,
+                                mAlarmIntent,
+                                PendingIntent.FLAG_CANCEL_CURRENT
+                        );
+                //알람을 위한 AlarmManager 선언 및 설정
+                //기존 세팅시간 + 화면꺼진시간으로 알람시간 지연시켜서 재등록
+                mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                mAlarmManager.set(
+                        AlarmManager.RTC_WAKEUP,
+                        time + durationOff,
+                        mPendingIntent
+                );
+                //세팅시간 변경(누적을 위해)
+                time = time + durationOff;
+                Log.d("test","changed_set_time: " + sdf.format(time));
+                Calendar temp = Calendar.getInstance();
+                temp.setTimeInMillis(time);
+
+            } else if (ScreenOffReceiver.SCREEN_OFF_NOTIFICATION.equals(intent.getAction())) {
+                //intent로 ScreenOff 정보 받기
+                presentOffTime = intent.getLongExtra("presentTime", 0 );
+                String offLatest = "Off Latest "+ intent.getStringExtra("present");
+                String offPast = "Off Past " + intent.getStringExtra("past");
+                Log.d("test", intent.getStringExtra("name")+" offReceive");
+                //화면 켜져있던 시간 구하기
+                durationOn = presentOffTime - presentOnTime;
+
+                //알람을 위한 Intent, PendingIntent 선언
+                Intent mAlarmIntent = new Intent("com.example.mission1.receiver.ALARM_ON");
+                PendingIntent mPendingIntent =
+                        PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                0,
+                                mAlarmIntent,
+                                PendingIntent.FLAG_CANCEL_CURRENT
+                        );
+                //알람을 위한 AlarmManager 선언 및 설정
+                //화면이 꺼졌으므로 미션 실패 알람을 취소(꺼져있을 때는 미션을 실패할 수 없다)
+                mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                mAlarmManager.cancel(mPendingIntent);
+                Log.d("test_off_cancel","alarm is canceled");
+                //timeThread.interrupt();
+            }
+            else if (AlarmReceiver.MISSTION_FAIL_NOTIFICATION.equals(intent.getAction())){
+                //알람을 위한 Intent, PendingIntent 선언
+                Intent mAlarmIntent = new Intent("com.example.mission1.receiver.ALARM_ON");
+                mAlarmIntent.putExtra("flag","fail");
+                PendingIntent mPendingIntent =
+                        PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                1,
+                                mAlarmIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                //알람을 위한 AlarmManager 선언 및 설정
+                //성공 알람 취소
+                mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                mAlarmManager.cancel(mPendingIntent);
+                //timeThread.interrupt();
+            }
+            else if (AlarmReceiver.MISSTION_SUCCESS_NOTIFICATION.equals(intent.getAction())){
+                //알람을 위한 Intent, PendingIntent 선언
+                Intent mAlarmIntent = new Intent("com.example.mission1.receiver.ALARM_ON");
+                mAlarmIntent.putExtra("flag","fail");
+                PendingIntent mPendingIntent =
+                        PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                0,
+                                mAlarmIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                //알람을 위한 AlarmManager 선언 및 설정
+                //실패 알람 취소
+                mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                mAlarmManager.cancel(mPendingIntent);
+                //timeThread.interrupt();
+            }
+        }
+    }
 }
